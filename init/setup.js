08325a76be407f60e21af860f3a24f6f4fb596e1
Jan Pospíšil <me@honzapospisil.com>
2021-11-12 19:33:52 +0100

    Fix sftp names

--- init/setup.js
+++ init/setup.js
@@ -21,15 +21,17 @@ const runScript = async () => {
   };
   const sftpProd = await question("Setup SFTP production connection? [Y/N]");
   if (sftpProd.toUpperCase() === "Y") {
-    data.SFTP_PROD_SERVER = await question("SFTP production host");
-    data.SFTP_PROD_PASSWORD = await question("SFTP production user");
-    data.SFTP_PROD_HOST = await question("SFTP production password");
+    data.SFTP_PROD_HOST = await question("SFTP production host");
+    data.SFTP_PROD_USER = await question("SFTP production user");
+    data.SFTP_PROD_PASSWORD = await question("SFTP production password");
+    data.SFTP_PROD_PORT = await question("SFTP production password");
   }
   const sftpDev = await question("Setup SFTP dev connection? [Y/N]");
   if (sftpDev.toUpperCase() === "Y") {
-    data.SFTP_DEV_SERVER = await question("SFTP dev host");
-    data.SFTP_DEV_PASSWORD = await question("SFTP dev user");
-    data.SFTP_DEV_HOST = await question("SFTP dev password");
+    data.SFTP_DEV_HOST = await question("SFTP dev host");
+    data.SFTP_DEV_USER = await question("SFTP dev user");
+    data.SFTP_DEV_PASSWORD = await question("SFTP dev password");
+    data.SFTP_PROD_PORT = await question("SFTP dev port");
   }
   // Lets replace vars to config file
   replaceVarsInFiles(data);
